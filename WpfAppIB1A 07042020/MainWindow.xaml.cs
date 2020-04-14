bbdd1f4e81f6a58cfd163202ee90953f6b80c958
finnrens <finnrens@outlook.com>
2020-04-14 13:17:01 +0200

    init

--- WpfAppIB1A 07042020/MainWindow.xaml.cs
+++ WpfAppIB1A 07042020/MainWindow.xaml.cs
@@ -24,33 +24,30 @@ namespace WpfAppIB1A_07042020
         public MainWindow()
         {
             InitializeComponent();
-            //Datagrid vullen met data vanuit de database
+         
             dgKoffie.ItemsSource = db.kofsmaaks.ToList();
         }
 
         private void BtnOpslaan_Click(object sender, RoutedEventArgs e)
         {
-            //Nieuw object aanmaken/instantieren
+         
             koffie deKoffie = new koffie();
             deKoffie.koffienaam = txtKoffie.Text;
 
-            //Nieuw object aanmaken/instantieren
+         
             smaak deSmaak = new smaak();
             deSmaak.smaaknaam = txtSmaak.Text;
 
-            //Nieuw object aanmaken met daarin het koffieobject en het smaakobject
+          
             kofsmaak deKofSmaak = new kofsmaak();
             deKofSmaak.koffie = deKoffie;
             deKofSmaak.smaak = deSmaak;
 
-            //InsertOnSubmit zet de toe te voegen data in de wachtrij 
-            //om opgeslagen te worden. Hij neemt nu zowel de koffie als 
-            //smaak mee in de database
             db.kofsmaaks.InsertOnSubmit(deKofSmaak);
-            //Daadwerkelijk opslaan
+            
             db.SubmitChanges();
 
-            //Bron aanpassen met nieuwe data
+            
             dgKoffie.ItemsSource = db.kofsmaaks.ToList();
 
             
